24964365781098562a957dc27e2fa3f83d764005
wangbo <wangbo156@foxmail.com>
2023-04-25 13:53:19 +0800

    refactor train_pipeline

--- main/run.py
+++ main/run.py
@@ -1,45 +1,18 @@
-import sys
-import json
 import time
 from train_pipeline import TrainPipeline
 
 
-def get_config():
-    data_config_path = sys.argv[1]
-    with open(data_config_path) as f:
-        config_str = f.read()
-        data_config = json.loads(config_str)
-
-    model_config_path = sys.argv[2]
-    with open(model_config_path) as f:
-        config_str = f.read()
-        model_config = json.loads(config_str)
-
-    config = {**data_config, **model_config}
-    return config
-
-
-def train():
-    model = pipeline.train()
-    model.summary()
+def save_model():
     date_time = time.strftime("%Y-%m-%d %H%M%S", time.localtime())
     model.save(f"..\\output\\{date_time}\\model")
     with open(f"..\\output\\{date_time}\\config.json", 'w', encoding='utf-8') as f:
-        f.write(res_config)
-
-
-def print_train_data():
-    ds = pipeline.read_data()
-    sample = next(iter(ds))
-    for k, v in sample[0].items():
-        print(f"{k}: {v}")
-    print(sample[1])
+        f.write(pipeline.config_str)
 
 
 if __name__ == '__main__':
-    config = get_config()
-    res_config = json.dumps(config)  # 序列化成json格式字符串
-    pipeline = TrainPipeline(config, run_eagerly=True)
-    print_train_data()
-    # train()
-    print(config)
+    data_config_path = "..\config\data_ali_ccp_test_v1.json"
+    model_config_path = "..\config\model_esmm.json"
+    pipeline = TrainPipeline(data_config_path, model_config_path, run_eagerly=True)
+    model = pipeline.train()
+    model.summary()
+    save_model()
--- main/train_pipeline.py
+++ main/train_pipeline.py
@@ -1,5 +1,5 @@
 import sys
-from collections import OrderedDict
+import json
 import tensorflow as tf
 from tensorflow.python.keras import optimizers
 from tensorflow.keras.optimizers.schedules import ExponentialDecay
@@ -10,24 +10,45 @@ from models.fm import FM
 from models.esmm import ESMM
 from models.din import DIN
 
-class TrainPipeline():
-    def __init__(self, config, run_eagerly=None):
-        self.config = config
+
+class TrainPipeline:
+    def __init__(self, data_config_path, model_config_path, run_eagerly=None):
+        with open(data_config_path) as f:
+            config_str = f.read()
+            data_config = json.loads(config_str)
+        with open(model_config_path) as f:
+            config_str = f.read()
+            model_config = json.loads(config_str)
+        self.config = {**data_config, **model_config}
+        self.config_str = json.dumps(self.config)
         self.run_eagerly = run_eagerly
 
-    def map_input(self, features):
-        def func(tensor_dict):
-            res = {}
-            for feature in features:
-                res[feature] = tensor_dict[feature]
-            return res
-        return func
+    def map_sample(self):
+        features = self.config["feature_config"]["features"]
+        pad_num = self.config["feature_config"]["pad_num"]
+        batch_size = self.config["data_config"]["batch_size"]
 
-    def map_feature(self, features, pad_num, batch_size):
-        def func(feature_tensor):
+        def get_label(tensor_dict):
+            labels = self.config["data_config"]["label_fields"]
+            if isinstance(labels, str):
+                return tensor_dict[labels]
+            elif isinstance(labels, list):
+                if len(labels) == 1:
+                    labels = labels[0]
+                    return tensor_dict[labels]
+                else:
+                    label_dict = {}
+                    for label_name in labels:
+                        label_dict[label_name] = tensor_dict[label_name]
+                    return label_dict
+            else:
+                raise ValueError(f"labels must be a string or list, but {labels}")
+
+        def func(sample_tensor_dict):
+            feature_dict = {}
             for feature in features:
                 if feature["feature_type"] == "WeightTagFeature":
-                    tensor = feature_tensor[feature["input_names"]]
+                    tensor = sample_tensor_dict[feature["input_names"]]
                     tensor = tf.strings.split(tf.strings.split(tensor, chr(1)), chr(2))
                     tensor = tensor.to_tensor(shape=[None, pad_num, 2], default_value='')
                     index, value = tf.split(tensor, num_or_size_splits=2, axis=2)
@@ -46,12 +67,13 @@ class TrainPipeline():
                     else:
                         raise ValueError(f"unexpected feature index_type{feature['index_type']}")
                 elif feature["feature_type"] == "TagFeature":
-                    index = feature_tensor[feature["input_names"]]
+                    index = sample_tensor_dict[feature["input_names"]]
                     index = tf.strings.split(index, chr(1))
                     index = index.to_tensor(shape=[None, pad_num], default_value='')
                     emp_str = tf.constant('', shape=[batch_size, pad_num])
                     value_0 = tf.equal(index, emp_str)
-                    value = tf.where(value_0, tf.zeros_like(index, dtype=tf.float32), tf.ones_like(index, dtype=tf.float32))
+                    value = tf.where(value_0, tf.zeros_like(index, dtype=tf.float32),
+                                     tf.ones_like(index, dtype=tf.float32))
                     if feature["index_type"] == "int":
                         index = tf.where(tf.equal(index, emp_str), tf.constant('0', shape=[batch_size, pad_num]), index)
                         index = tf.strings.to_number(index, out_type=tf.int32)
@@ -61,35 +83,13 @@ class TrainPipeline():
                     else:
                         raise ValueError(f"unexpected feature index_type{feature['index_type']}")
                 else:
-                    index = feature_tensor[feature["input_names"]]
+                    index = sample_tensor_dict[feature["input_names"]]
                     index = tf.expand_dims(index, axis=1)
-                    res = {"index": index,
-                           "value": tf.ones_like(index, dtype=tf.float32)}
-                feature_tensor[feature["input_names"]] = res
-            return feature_tensor
-        return func
+                    res = {"index": index, "value": tf.ones_like(index, dtype=tf.float32)}
+                feature_dict[feature["input_names"]] = res
+            return feature_dict, get_label(sample_tensor_dict)
 
-    def get_label(self, labels):
-        def get_label(tensor_dict):
-            label = tensor_dict[labels]
-            del tensor_dict[labels]
-            return tensor_dict, label
-        def get_multi_label(tensor_dict):
-            label_dict = {}
-            for label_name in labels:
-                label_dict[label_name] = tensor_dict[label_name]
-                del tensor_dict[label_name]
-            return tensor_dict, label_dict
-        if isinstance(labels, str):
-            return get_label
-        elif isinstance(labels, list):
-            if len(labels) == 1:
-                labels = labels[0]
-                return get_label
-            else:
-                return get_multi_label
-        else:
-            raise ValueError(f"labels must be a string or list, but {labels}")
+        return func
 
     def read_data(self):
         data_config = self.config["data_config"]
@@ -103,12 +103,7 @@ class TrainPipeline():
         else:
             print(f"unexpected input_type: {data_config['input_type']}")
             sys.exit(1)
-        feature_label_list = [feature["input_names"] for feature in self.config["feature_config"]["features"]]
-        feature_label_list.extend(self.config["data_config"]["label_fields"])
-        train_ds = train_ds.map(self.map_input(feature_label_list), num_parallel_calls=4)
-        pad_num = self.config["feature_config"]["pad_num"]
-        train_ds = train_ds.map(self.map_feature(self.config["feature_config"]["features"], pad_num, data_config["batch_size"]), num_parallel_calls=4)
-        train_ds = train_ds.map(self.get_label(data_config["label_fields"]), num_parallel_calls=4)
+        train_ds = train_ds.map(self.map_sample(), num_parallel_calls=4)
         train_ds = train_ds.prefetch(data_config["prefetch_size"])
         return train_ds
 
@@ -169,3 +164,14 @@ class TrainPipeline():
             steps_per_epoch=self.config["train_config"]["steps_per_epoch"],
         )
         return model
+
+
+if __name__ == '__main__':
+    # 预览训练样本
+    pipeline = TrainPipeline("..\config\data_ali_ccp_test_v1.json", "..\config\model_esmm.json", run_eagerly=True)
+    ds = pipeline.read_data()
+    sample = next(iter(ds))
+    for feature_name, feature_tensor in sample[0].items():
+        print(f"{feature_name}: {feature_tensor}")
+    print(sample[1])
+    print(sample)
